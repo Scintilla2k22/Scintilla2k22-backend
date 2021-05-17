7c7b52d676a06d443c4840d6a7a380c8d91c3c12
ayushbisht2001 <bisht.sanu2001@gmail.com>
2021-05-17 10:41:14 -0700

    debuggin 6

--- covid_backend/patient/urls.py
+++ covid_backend/patient/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('get_patient_profile/<str:id>/', get_patient_profile, name="get_patient_profile"),
     path('get_alloted_beds/', get_alloted_beds, name="get_alloted_beds"),
     path('change_patient_status/<str:id>/', change_patient_status, name="change_patient_status"),
-    path('patient_migration/', patient_migration, name="patient_migration"),
+    path('patient_status/', patient_status, name="patient_status"),
     path('get_filter_patients/<str:status>/', get_filter_patients, name="get_filter_patients"),
     path('get_searched_patients/<str:query>/', get_searched_patients, name="get_searched_patients")
 ]
