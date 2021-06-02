97b2ba09c2f5849122b5e785887db08044568c2f
ayushbisht2001 <bisht.sanu2001@gmail.com>
2021-06-02 08:16:54 -0700

    patient health paginated

--- covid_backend/health/admin.py
+++ covid_backend/health/admin.py
@@ -8,9 +8,9 @@ from django.contrib import auth
 class HealthStatusCustom(admin.ModelAdmin):
     search_fields = ('patient','patient_condition')
 
-    def has_add_permission(self, request): 
-        return False
-    def has_change_permission(self, request, obj=None):
-        return False
+#     def has_add_permission(self, request): 
+#         return False
+#     def has_change_permission(self, request, obj=None):
+#         return False
 
-admin.site.unregister(auth.models.Group)
+# admin.site.unregister(auth.models.Group)
--- covid_backend/health/urls.py
+++ covid_backend/health/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     # path('register/', PatientAdmissionView.as_view(), name="register"),
     path('update/', PatientHealthUpdateView.as_view(), name="update"),
     path('details/<str:icmr>/', get_patients_health, name="get_patients_health"),
+
+    # paginated data 
+    path('patient_health_list/<str:icmr>/', PatientHealthStatusList.as_view(), name="patient_health_list")
+
 ]
