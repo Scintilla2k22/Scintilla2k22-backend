5b7f0f29cffc0a0e75c085e7bdeabfe47bb17193
ayushbisht2001 <bisht.sanu2001@gmail.com>
2021-06-02 01:22:03 -0700

    all set

--- covid_backend/patient/paginations.py
+++ covid_backend/patient/paginations.py
@@ -3,4 +3,4 @@ from rest_framework.pagination import PageNumberPagination
 class PatientListPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
-    max_page_size = 50
+    max_page_size = 100
