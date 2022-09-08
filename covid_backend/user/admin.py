07f367f4035ddec8f55068b5a14c96c5eaf348ed
Ayush Bisht <bisht.ayush2001@gmail.com>
2022-09-08 18:59:49 +0530

    update

--- covid_backend/user/admin.py
+++ covid_backend/user/admin.py
@@ -32,6 +32,7 @@ class MedicalStaffProfileAdmin(admin.ModelAdmin):
 
 @admin.register(MedicalStaffProfile)
 class MedicalStaffProfileCustom(admin.ModelAdmin):
+    pass
     def has_add_permission(self, request): 
         return False
     def has_change_permission(self, request, obj=None):
